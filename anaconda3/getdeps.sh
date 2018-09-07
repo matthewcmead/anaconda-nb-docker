8611ad65502e4fc38810868b7fb9447e74f142b9
matthewcmead <matthewcmead@gmail.com>
2018-09-06 21:54:41 -0400

    update to 5.2.0

--- anaconda3/getdeps.sh
+++ anaconda3/getdeps.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-(cd software_dist && wget --quiet https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh)
+#(cd software_dist && wget --quiet https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh)
+(cd software_dist && wget https://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh)
 (cd bin && TINI_VERSION=`curl https://github.com/krallin/tini/releases/latest | grep -o "/v.*\"" | sed 's:^..\(.*\).$:\1:'` && curl -L https://github.com/krallin/tini/releases/download/v${TINI_VERSION}/tini >tini)
